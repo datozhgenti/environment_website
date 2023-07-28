c33a9cdf2bdcc026b32e44c6ac9b82eb6757bb5c
datozhgenti <82455319+datozhgenti@users.noreply.github.com>
2023-07-28 17:32:08 +0400

    preload issue fix

--- js/721-legacy.6dfa525e.js
+++ js/721-legacy.6dfa525e.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkenvironment_website"]=self["webpackChunkenvironment_website"]||[]).push([[721],{1721:function(e,n,i){i.r(n),i.d(n,{default:function(){return c}});var r=i(3396),s={class:"secondary-text line-height-26"};function t(e,n,i,t,u,o){return(0,r.wg)(),(0,r.iD)("p",s," Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla ")}var u={},o=i(89);const a=(0,o.Z)(u,[["render",t]]);var c=a}}]);
-//# sourceMappingURL=721-legacy.2bff4573.js.map
+//# sourceMappingURL=721-legacy.6dfa525e.js.map
